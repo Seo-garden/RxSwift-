e03cde1c271c31eb49b1b7142fd9014121aedf1c
seo-garden <indian4589@naver.com>
2024-08-30 17:58:09 +0900

    RxSwift 복습

--- Example/season2/step1/RxSwift+MVVM/ViewController.swift
+++ Example/season2/step1/RxSwift+MVVM/ViewController.swift
@@ -1,7 +1,16 @@
-//1시간 30분~ disposeBag 부터 다시 
 import RxSwift
 import SwiftyJSON
 import UIKit
+
+//MARK: - 정리
+//우리는 비동기로 생성되는 데이터를 취급하기 위해 컴플리션으로 전달했었는데, return 값으로 전달해주고 싶어서 RxSwift 를 쓴다.
+//1. RxSwift 비동기적으로 생기는 결과를 컴플리션으로 전달하는게 아니라 return 값으로 전달하기 위한 유틸리티다.
+//2. Observable로 오는 데이터를 받아서 return 하는 방법 or 비동기로 생기는 데이터를 Observable 로 감싸서 return 하는 방법이 있다.
+//2-1. Observable 의 생명주기는 create() -> subscribe(on: ) -> onNext() or onCompleted() or onError() -> disposed()
+//2-2. 여기서 onCompleted() 혹은 onError() 가 오게 되면 클로저가 종료되고 재사용이 불가능해진다.
+//3. .debug()    .subscribe 전의 데이터가 전달되는 동안 전달되는 데이터를 다 찍어낼 수 있다.
+//4. .observe(on: MainScheduler.instance) 메인쓰레드로 변경되는데, subscribe 라던지 아래에 오는 코드(다운스트림) 모두 메인쓰레드에서 실행된다.
+//5. onError or onCompleted() 를 하게 되면 클로저가 해제되면서 메모리가 해제되는데, 아래의 코드 경우 onNext() 처리를 해주고 disposeBag 을 해주어야 메모리가 해제된다.
 
 let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=44ce18f0"
 
@@ -18,6 +27,7 @@ let MEMBER_LIST_URL = "https://my.api.mockaroo.com/members_with_avatar.json?key=
 //}
 
 class ViewController: UIViewController {
+    
     @IBOutlet var timerLabel: UILabel!
     @IBOutlet var editView: UITextView!
     
@@ -46,8 +56,8 @@ class ViewController: UIViewController {
     //@escaping 왜 필요한가? -> 본체함수가 실행 끝난 후에 실행이 되니까 지정을 해줘야 한다. 만약 반환타입이 옵셔널일 경우에는 escaping 이 자동으로
     
     // Observable 의 생명주기 :
-    //1. create
-    //2. subscribe
+    //1. create 로 만들고
+    //2. subscribe 로 받아서
     //3. onNext
     //------끝----- 이 난다는게 재사용이 불가능하다.
     //4. onCompleted / onError
@@ -106,6 +116,7 @@ class ViewController: UIViewController {
         setVisibleWithAnimation(activityIndicator, true)
         
         //2. Observable 로 오는 데이터로 받아서 처리하는 방법
+        //아래와 같이 표현할 수 있지만, 너무 귀찮다. 그래서 여러가지 sugar API 를 제공한다. 그걸 오퍼레이터라고 부른다.
 //        _ = downloadJson(MEMBER_LIST_URL)
 //            .debug()        //.subscribe 전의 데이터가 전달되는 동안 전달되는 데이터를 다 찍어낼 수 있다.
 //            .subscribe { event in       //subscribe 가 나중에 오면 호출한다.      [weak self]
@@ -127,7 +138,8 @@ class ViewController: UIViewController {
         
         Observable.zip(jsonObservable, helloObservable) { $1 + "'\n'" + $0 }
             .observe(on: MainScheduler.instance)        //super: operator       다운스트림의 쓰레드를 변경
-            .subscribe(onNext: { json in                //업스트림의 쓰레드를 변경
+        //MainScheduler.instane 를 선언후에 subscribe 를 하면 모든 동작이 DispatchQueue.main.async 에서 실행된다.
+            .subscribe(onNext: { json in                //업스트림의 쓰레드를 변경 시작쓰레드의 영향을 준다.
                 self.editView.text = json
                 self.setVisibleWithAnimation(self.activityIndicator, false)
             })
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # 곰튀김 RxSwift
 
-
+우리는 비동기로 생성되는 데이터를 취급하기 위해 컴플리션으로 전달했었는데, return 값으로 전달해주고 싶어서 RxSwift 를 쓴다.  
+1. RxSwift 비동기적으로 생기는 결과를 컴플리션으로 전달하는게 아니라 return 값으로 전달하기 위한 유틸리티다.  
+2. Observable로 오는 데이터를 받아서 return 하는 방법 or 비동기로 생기는 데이터를 Observable 로 감싸서 return 하는 방법이 있다.  
+2-1. Observable 의 생명주기는 create() -> subscribe(on: ) -> onNext() or onCompleted() or onError() -> disposed()  
+2-2. 여기서 onCompleted() 혹은 onError() 가 오게 되면 클로저가 종료되고 재사용이 불가능해진다. 클로저가 종료되므로 자동으로 메모리가 해제된다.  
+3. .debug()    .subscribe 전의 데이터가 전달되는 동안 전달되는 데이터를 다 찍어낼 수 있다.  
+4. .observe(on: MainScheduler.instance) 메인쓰레드로 변경되는데, subscribe 라던지 아래에 오는 코드(다운스트림) 모두 메인쓰레드에서 실행된다.  
+5. onError or onCompleted() 를 하게 되면 클로저가 해제되면서 메모리가 해제되는데, onError(), onCompleted() 를 선언하지 않고 사용하게 될 경우, 메모리가 해제되지 않는데, onNext() 처리를 해주고 disposeBag 을 해주면 메모리가 해제된다.  
